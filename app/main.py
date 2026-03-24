303f00e41fb7d1037a6c005eb74b201ebeec55ce
Volodymyr Stashenko <volodyasilver@gmail.com>
2026-03-24 22:50:50 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,24 +1,22 @@
 from app.knight import Knight
 
 
+def conduct_fight(k1: Knight, k2: Knight) -> None:
+    k1_power = k1.power
+    k2_power = k2.power
+
+    k1.take_damage(k2_power)
+    k2.take_damage(k1_power)
+
+
 def battle(knights_config: dict) -> dict:
     # Створюємо об'єкти лицарів
     lancelot = Knight(knights_config["lancelot"])
     arthur = Knight(knights_config["arthur"])
     mordred = Knight(knights_config["mordred"])
     red_knight = Knight(knights_config["red_knight"])
-
-    lancelot_power = lancelot.power
-    mordred_power = mordred.power
-
-    lancelot.take_damage(mordred_power)
-    mordred.take_damage(lancelot_power)
-
-    arthur_power = arthur.power
-    red_knight_power = red_knight.power
-
-    arthur.take_damage(red_knight_power)
-    red_knight.take_damage(arthur_power)
+    conduct_fight(lancelot, mordred)
+    conduct_fight(arthur, red_knight)
 
     return {
         lancelot.name: lancelot.hp,
